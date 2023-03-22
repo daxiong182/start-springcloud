4266ff034a813e1742ca482c31e8fc8ceaa1f35c
德龙 <2043693318@qq.com>
2023-03-23 03:03:33 +0800

    spring-cloud 远程配置

---
+++ zipkin/src/main/java/com/courage/ZipKinApplication.java
@@ -0,0 +1,14 @@
+package com.courage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class,args);
+    }
+
+}
